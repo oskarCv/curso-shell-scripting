22bf0315bacbf9bb4bdd0c5988eb98de11ddeb99
oskacr Cv <oskar.cv91@gmail.com>
2022-10-25 01:49:43 -0500

    Adding functions intro

---
+++ Modulo-01/04-Intro-Funciones.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "1: Bienvenidos al curso de bash Shell Scripting"
+
+function mensaje_bienvenida1 {
+   echo "2: Bienvenidos al curso de bash Shell Scripting" 
+}
+
+mensaje_bienvenida2() {
+   echo "3: Bienvenidos al curso de bash Shell Scripting"  
+}
+
+mensaje_bienvenida2
+mensaje_bienvenida1
