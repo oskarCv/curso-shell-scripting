7b0537de5efe6f5ea87cff3e98a75dfd42fea11b
oskacr Cv <oskar.cv91@gmail.com>
2022-11-06 19:00:37 -0600

    Adding scripts

---
+++ Modulo-01/05-printf-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#printf "Mensaje !!!\n"
+#printf "2a Mensaje !!!\n" "2b Mensaje !!!\n" "2c Mensaje !!!\n"
+#printf "%sABC\n" "3a Mensaje !!!" "3b Mensaje !!!" "3c Mensaje !!!"
+
+printf "%-25s:%4d\n" "Curso de Shell Scripting" 400 "Curso de Java" 400 "Curso de HTML" 4000
